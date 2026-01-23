73883747f938c97967ab1fff9484e1a7f1c93896
rex-roller-dev <hhgfdzjokhg@gmail.com>
2026-01-24 01:21:09 +0800

    [文档]修改了一些变量的定义类型，加了一些无关紧要的注释

--- listen_form_data.py
+++ listen_form_data.py
@@ -2,17 +2,17 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-def write_to_file(text):
+def write_to_file(text):#重定向输出到文件
     with open("test.txt", "a", encoding="utf-8") as f:
         f.write(text + "\n")
 
 @app.route("/wps_callback", methods=["POST"])
 def wps_callback():
-    data = request.get_json()
+    data : dict = request.get_json()
     print("收到：", data)
 
     # 重定向输出到文件
-    write_to_file(str(data))
+    #write_to_file(str(data))
 
     return jsonify({"code": 0, "msg": "ok"}), 200
 
