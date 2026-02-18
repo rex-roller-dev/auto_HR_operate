1df9d7598f3d2cba2fac7db61edc5fd96bf3306d
rex-roller-dev <hhgfdzjokhg@gmail.com>
2026-02-19 00:37:19 +0800

    [bug]

---
+++ email_config.py
@@ -0,0 +1,7 @@
+# email_config.py
+
+SMTP_SERVER = "smtp.exmail.qq.com"
+SMTP_PORT = 465
+SENDER_EMAIL = "2024270025@mails.szu.edu.cn"      # 企业邮箱完整地址
+SENDER_PASSWORD = "Aa07060706"          # 不是授权码
+SENDER_NAME = "志愿服务系统"
