581d1a653515beeea54c320585aa7af0d70e87c3
rex-roller-dev <hhgfdzjokhg@gmail.com>
2026-03-16 12:38:18 +0800

    [bug]新增了只有深大义工的时候也可以申请

--- volunteer_hours_verify.py
+++ volunteer_hours_verify.py
@@ -26,15 +26,16 @@ def volunteer_hours_verify(
     cert_szu_hours = certificate_data['szu_volunteer_hours']
 
     # 1️⃣ 姓名
-    if sz_volunteer_data['姓名'] and sz_volunteer_data['义工号'] and sz_volunteer_data['服务时长'] is None:
-        raise  FileError(
-            f"志愿深圳数据错误，请从服务明细-导出明细中获取文件，详情请查看公众号"
-            )
+    if sz_volunteer_data is not None:
+        if sz_volunteer_data['姓名'] is None or sz_volunteer_data['义工号'] is None or sz_volunteer_data['服务时长'] is None:
+            raise  FileError(
+                f"志愿深圳数据错误，请从服务明细-导出明细中获取文件，详情请查看公众号"
+                )
     
-    if sz_volunteer_data['姓名'] != name:
-        raise NameMismatchError(
-            f"姓名不匹配: 证书({name}) vs 志愿深圳({sz_volunteer_data['姓名']})"
-        )
+        if sz_volunteer_data['姓名'] != name:
+            raise NameMismatchError(
+                f"姓名不匹配: 证书({name}) vs 志愿深圳({sz_volunteer_data['姓名']})"
+            )
 
     # 2️⃣ 志愿深圳
     if cert_sz_hours is not None:
